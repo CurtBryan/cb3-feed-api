b230491191ad4acc28be15a5b00006e87d8e9c63
Curt Bryan <curt.bryan@phoenix.edu>
2021-05-13 14:02:44 -0700

    123123jhgk

--- src/server.ts
+++ src/server.ts
@@ -38,16 +38,17 @@ import { V0_FEED_MODELS } from "./controllers/v0/model.index";
     );
 
     app.use(function (req, res, next) {
-      res.header(
-        "Access-Control-Allow-Origin",
-        "*"
-      );
+      res.header("Access-Control-Allow-Origin", "*");
       res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token"
       );
       next();
     });
+
+    var cors = require("cors");
+
+    app.use(cors()); // Use this after the variable declaration
 
     app.use("/api/v0/", IndexRouter);
 
